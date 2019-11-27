566dc023ed404564ae55ec7d75a46eac56ce5945
Florian <fbaudry@epsi.fr>
2019-11-27 20:12:24 +0100

    fix team scrapping

--- bin/js/vendor/teams.js
+++ bin/js/vendor/teams.js
@@ -4,11 +4,11 @@ const puppeteer = require('puppeteer');
 const start = async(league) => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto(league.link + 'equipes');
+    await page.goto(league.link + 'classement');
     await page.waitFor(3000);
 
     const resultats = await page.evaluate(() => {
-        let leagues = Array.from(document.querySelectorAll('#tournament-page-participants .leagueTable__team'));
+        let leagues = Array.from(document.querySelectorAll('#table-type-1 .team_name_span'));
 
         return leagues.map(league => {
             return league.innerText;
